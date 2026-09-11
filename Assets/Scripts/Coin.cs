df677326c69015ae78e83b338be3fa7237c76970
theboiirooii <phiwokuhlevanrooii@gmail.com>
2026-09-11 20:59:07 +0200

    added Section B

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,6 +6,8 @@ public class Coin : MonoBehaviour
     public int value;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!other.CompareTag("Player")) return;
+
         GameMNGR.Instance.IncrementScore(value);
         Destroy(this.gameObject);
     }
--- Assets/Scripts/WinOnCollission.cs
+++ Assets/Scripts/WinOnCollission.cs
@@ -1,23 +1,14 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WinOnCollission : MonoBehaviour
 {
-    private string sceneName;
-
-    private void Start()
-    {
-        sceneName = SceneManager.GetActiveScene().name;
-    }
-
     private void OnTriggerEnter2D(Collider2D trig)
     {
         if (trig.gameObject.CompareTag("Player"))
         {
-            Debug.Log("LEVEL FINISHED");
-            SceneManager.LoadScene(sceneName);
+            Debug.Log("LEVEL FINISHED - YOU WIN!");
+            Time.timeScale = 0f;
         }
     }
-
 }
